b535521960d2f165613fdfe2349630e4f1eb1d70
luccalkm <lucca.kleinsorgen@gmail.com>
2023-10-25 13:37:23 -0300

    Removing appsettings from git and first migration

--- AuctionService/Program.cs
+++ AuctionService/Program.cs
@@ -1,7 +1,14 @@
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddDbContext<AuctionDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
