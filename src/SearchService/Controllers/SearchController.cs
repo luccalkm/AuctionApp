6f60836790519e0703be98c9afa97801f13aae19
luccalkm <lucca.kleinsorgen@gmail.com>
2023-10-25 23:46:23 -0300

    Search params on mongodb

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MongoDB.Entities;
 using SearchService.Models;
+using SearchService.RequestHelpers;
 
 namespace SearchService.Controllers;
 
@@ -9,19 +10,64 @@ namespace SearchService.Controllers;
 public class SearchController : ControllerBase
 {
     [HttpGet]
-    public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm)
+    public async Task<ActionResult<List<Item>>> SearchItems([FromQuery] SearchParams searchParams)
     {
-        var query = DB.Find<Item>();
-
-        query.Sort(x => x.Ascending(a => a.Make));
+        var query = DB.PagedSearch<Item, Item>();
         
-        if(!string.IsNullOrEmpty(searchTerm))
+        // Full search by text
+        if(!string.IsNullOrEmpty(searchParams.SearchTerm))
         {
-            query.Match(Search.Full, searchTerm).SortByTextScore();
+            query.Match(Search.Full, searchParams.SearchTerm).SortByTextScore();
         }
+
+        switch (searchParams.OrderBy)
+        {
+            case "make":
+                query.Sort(x => x.Ascending(a => a.Make));
+                break;
+            case "new":
+                query.Sort(x => x.Descending(a => a.CreatedAt));
+                break;
+            default:
+                query.Sort(x => x.Ascending(a => a.AuctionEnd));
+                break;
+        }
+
+        switch (searchParams.FilterBy)
+        {
+            case "finished":
+                query.Match(x => x.AuctionEnd <= DateTime.UtcNow);
+                break;
+            case "endingSoon":
+                query.Match(x => x.AuctionEnd < DateTime.UtcNow.AddHours(6) && x.AuctionEnd > DateTime.UtcNow);
+                break;
+            default:
+                query.Match(x => x.AuctionEnd > DateTime.UtcNow);
+                break;
+        }
+
+        // Filter Seller
+        if (!string.IsNullOrEmpty(searchParams.Seller))
+        {
+            query.Match(x => x.Seller == searchParams.Seller);
+        }
+        
+        // Filter winner
+        if (!string.IsNullOrEmpty(searchParams.Winner))
+        {
+            query.Match(x => x.Winner == searchParams.Winner);
+        }
+
+        query.PageNumber(searchParams.PageNumber);
+        query.PageSize(searchParams.PageSize);
 
         var result = await query.ExecuteAsync();
 
-        return result;
+        return Ok(new
+        {
+            results = result.Results,
+            pageCount = result.PageCount,
+            totalCount = result.TotalCount
+        });
     }
 }
