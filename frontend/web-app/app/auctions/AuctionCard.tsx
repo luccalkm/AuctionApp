4e1c74a3cd3a3d3823c80cfd6314db995877ea10
luccalkm <lucca.kleinsorgen@gmail.com>
2023-12-27 18:39:38 +0000

    starting frontend

---
+++ frontend/web-app/app/auctions/AuctionCard.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+import React from 'react'
+
+type Props = {
+    auction: any
+}
+
+export default function AuctionCard({auction}: Props) {
+  return (
+    <a href='#'>
+        <div className="w-full bg-gray-200 aspect-w-16 aspect-h-10 rounded-lg overflow-hidden">
+            <Image 
+                src={auction.imageUrl} 
+                alt='image'
+                fill
+                className='object-cover'
+             />
+        </div>
+    </a>
+  )
+}
---
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { AiOutlineCar } from 'react-icons/ai';
+
+export default function Navbar() {
+  return (
+    <header className="sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md">
+      <div className="flex items-center gap-2 text-3xl font-semibold text-red-500">
+        <AiOutlineCar size={34} />
+        <div>Bid Wheels</div>
+      </div>
+      <div>TODO: Search</div>
+      <div>TODO: Login</div>
+    </header>
+  );
+}
