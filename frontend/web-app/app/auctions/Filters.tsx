aa428ce7a5a7ceb1e625f6160124ce84487a028b
luccalkm <lucca.kleinsorgen@gmail.com>
2023-12-28 22:27:55 +0000

    add: sorting functionality

--- frontend/web-app/app/auctions/Filters.tsx
+++ frontend/web-app/app/auctions/Filters.tsx
@@ -1,27 +1,58 @@
 import { useParamsStore } from "@/hooks/useParamsStore";
 import { Button, ButtonGroup } from "flowbite-react";
 import React from "react";
+import { IconBase } from "react-icons";
+import { AiOutlineClockCircle, AiOutlineSortAscending } from "react-icons/ai";
+import { BsFillStopCircleFill } from "react-icons/bs";
 
-// type Props = {
-//   pageSize: number;
-//   setPageSize: (size: number) => void;
-// };
-
+const orderButtons = [
+  {
+    label: "Alphabetical",
+    icon: AiOutlineSortAscending,
+    value: "make",
+  },
+  {
+    label: "End date",
+    icon: AiOutlineClockCircle,
+    value: "endingSoon",
+  },
+  {
+    label: "Recently Added",
+    icon: BsFillStopCircleFill,
+    value: "new",
+  },
+];
 const pageSizeButtons = [4, 8, 12];
 
 export default function Filters() {
   const pageSize = useParamsStore((state) => state.pageSize);
-  const setParams = useParamsStore((state) => state.setParams);
+  const { setParams, orderBy } = useParamsStore();
 
   return (
     <div className="flex justify-between items-center mb-4">
+      <div>
+        <span className="uppercase text-sm text-gray-500 mr-2">Order By</span>
+        <ButtonGroup>
+          {orderButtons.map(({ label, icon: Icon, value }) => (
+            <Button
+              key={value}
+              onClick={() => setParams({ orderBy: value })}
+              color={`${orderBy === value ? "red" : "gray"}`}
+              className="focus:ring-0"
+            >
+              <Icon className="mr-3 h-4 w-4" />
+              {label}
+            </Button>
+          ))}
+        </ButtonGroup>
+      </div>
       <div>
         <span className="uppercase text-sm text-gray-500 mr-2">Page Size</span>
         <ButtonGroup>
           {pageSizeButtons.map((value, index) => (
             <Button
               key={index}
-              onClick={() => setParams({pageSize: value})}
+              onClick={() => setParams({ pageSize: value })}
               color={`${pageSize === value ? "red" : "gray"}`}
               className="focus:ring-0"
             >
